c54bde21e8cef79be98186c938b3b50816ac3956
Andre Hadianto <andrehadiantolesmana@gmail.com>
2020-12-07 17:31:06 +0800

    fix: replace jest setup import declaration

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,8 @@
 import "react-native-gesture-handler/jestSetup";
 import "@testing-library/jest-native/extend-expect";
 import "isomorphic-fetch";
-import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
+
+const mockAsyncStorage = require("@react-native-async-storage/async-storage/jest/async-storage-mock");
 
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 jest.mock("@expo/vector-icons", () => ({
